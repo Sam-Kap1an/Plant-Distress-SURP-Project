23856622056c875d73728ab2354cfb4ab528fa5c
sfkaplan <sfkaplan@calpoly.edu>
2024-06-28 13:15:25 -0700

    plotwave frm

--- preprocessor.py
+++ preprocessor.py
@@ -1,7 +1,9 @@
 import wave
 import numpy as np
-from scipy import signal
 import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
+
 
 #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.spectrogram.html
 #https://en.wikipedia.org/wiki/WAV
@@ -10,22 +12,18 @@ import matplotlib.pyplot as plt
 def getfilepath():
     return input("specify file path to wav file")
 
-#extract wave into a numpy array
-signal_wave = wave.open(getfilepath(), 'r')
-sample_frequency = signal_wave.getframerate()
-
-data = np.fromstring(signal_wave.readframes(sample_frequency), dtype=np.int16)
-sig = signal_wave.readframes(-1)
-
-sig = np.fromstring(sig, 'Int16')
-#sig = sig[:]
+# Load the WAV file
+sample_rate, data = wavfile.read(getfilepath())
 
 
+time = np.linspace(0, len(data) / sample_rate, num=len(data))
 
-#lot the data
-plt.figure(1)
-c = plt.subplot(211)
-Pxx, freqs, bins, im = c.specgram(sig, NFFT=1024, Fs=sample_frequency, noverlap=900)
-c.set_xlabel('Time')
-c.set_ylabel('Frequency')
+
+# Plot the waveform
+plt.figure(figsize=(10, 4))
+plt.plot(time, data)
+plt.title('Waveform')
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+plt.grid(True)
 plt.show()
